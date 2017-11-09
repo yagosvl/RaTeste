81a4f8debc6288f089ebdba8dcfd477c590e91a8
yagosvl <yagosvl@hotmail.com>
2017-11-09 19:50:09 -0300

    Copiado estrutura pré montada com as duas telas e dois marcadores

---
+++ Teaser01/Assets/CustomEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CustomEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            GameObject.Find("Canvas").GetComponent<PanelController>().SwitchPanel(mTrackableBehaviour.gameObject.name);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Teaser01/Assets/PanelController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DeactivatePanels();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SwitchPanel(string panelName)
+    {
+        Debug.Log("----------" + panelName);
+        int numberOfChildren = GameObject.Find("Canvas").transform.childCount;
+        for(int i = 0; i < numberOfChildren; i++)
+        {
+            GameObject.Find("Canvas").transform.GetChild(i).gameObject.SetActive(false);
+        }
+        GameObject.Find("Canvas").transform.GetChild(int.Parse(panelName)).gameObject.SetActive(true);
+        GameObject.Find("Canvas").transform.GetChild(2).gameObject.SetActive(true); //this is the button
+    }
+
+    private void DeactivatePanels()
+    {
+        Debug.Log("Passou pelo Deactivate");
+        int numberOfChildren = GameObject.Find("Canvas").transform.childCount;
+        for (int i = 0; i < numberOfChildren; i++)
+        {
+            GameObject.Find("Canvas").transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    public void CloseScreen()
+    {
+        GameObject.FindWithTag("m1").SetActive(false);
+        GameObject.Find("Canvas").transform.GetChild(2).gameObject.SetActive(false); //this is the button
+    }
+}
